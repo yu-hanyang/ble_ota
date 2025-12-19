bca847cadb2dfc56f07c97b18b6c273fbee692ce
yuhanyang <kobe__yu@yeah.net>
2025-12-19 14:06:02 +0800

    fix:优化app_main的代码结构

--- main/app_main.c
+++ main/app_main.c
@@ -24,31 +24,16 @@
 
 static const char * TAG = "main";
 
-static void print_task(void * arg)
-{
-    int cnt = 0;
-    while (1)
-    {
-        ESP_LOGI(TAG, "Free heap size: %d", cnt++);
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
-}
 
-static void print_task_init(void)
+void app_main(void)
 {
-    xTaskCreate(&print_task, "print_task", 2048, NULL, 5, NULL);
-}
-
-
-void
-app_main(void)
-{
+    ESP_LOGI(TAG, "+++++++++++app_main start+++++++++++");
     app_controller_task_init();
 
     app_nvs_init();
     app_ble_task_init();
     app_ota_task_init();//使用该任务前蓝牙和flash得先启动
-    print_task_init();
+
 
     app_led_task_init();
 }
